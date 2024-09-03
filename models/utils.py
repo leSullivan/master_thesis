7a70f21d0c3c725eefdfd8ac9fa1c03b9f6077fa
John Jänckel <john.jaenckel@elunic.com>
2024-09-03 16:13:19 +0200

    fix: dino device

--- models/utils.py
+++ models/utils.py
@@ -42,6 +42,7 @@ class VitExtractor:
     def __init__(self, model_name, device):
         # pdb.set_trace()
         self.model = torch.hub.load("facebookresearch/dino:main", model_name).to(device)
+        self.device = device
         self.model.eval()
         self.model_name = model_name
         self.hook_handlers = []
@@ -145,6 +146,7 @@ class VitExtractor:
         return feature
 
     def get_qkv_feature_from_input(self, input_img):
+        input_img = input_img.to(self.device)
         self._register_hooks()
         self.model(input_img)
         feature = self.outputs_dict[VitExtractor.QKV_KEY]

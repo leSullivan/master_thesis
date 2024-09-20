6ed31198b97fd2cc048b3631c79a88964ab4d021
John Jänckel <john.jaenckel@elunic.com>
2024-09-20 14:59:40 +0200

    chore: change test queue

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# for g_type in resnet-6 unet; do
-#   for d_type in basic patch pixel; do
-    # sbatch --export=D_TYPE=$d_type,G_TYPE=$g_type slurm_template.sh
-#   done
-# done
-
-sbatch --export=D_TYPE=basic,G_TYPE=resnet-6 slurm_template.sh
+ for model_name in cgan cyclegan turbo_cyclegan; do 
+    sbatch --export=MODEL_NAME=$model_name slurm_template.sh
+ done
--- slurm_template.sh
+++ slurm_template.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=gan_exploration    
 #SBATCH --partition=clara
-#SBATCH --output=output_%j.txt    
-#SBATCH --error=error_%j.txt          
+#SBATCH --output=slurm_res/output_%j.txt    
+#SBATCH --error=slurm_res/error_%j.txt          
 #SBATCH --ntasks=1                    
 #SBATCH --cpus-per-task=4            
 #SBATCH --mem=32G                               
-#SBATCH --gres=gpu:v100:1           
+#SBATCH --gres=gpu:v100:1         
 #SBATCH --time=48:00:00
 #SBATCH --mail-user=john.jaenckel@icloud.com
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-# Load necessary modules (example)
-module r default
+# Load necessary modules
+source .venv/bin/activate
 
 # Run your training script
-srun python train.py --d_type=$D_TYPE --g_type=$G_TYPE
+srun python main.py --model_name=$MODEL_NAME

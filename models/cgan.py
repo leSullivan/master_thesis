9e096c73db2680128120c132f171ae1c2320d6e4
John Jänckel <john.jaenckel@elunic.com>
2024-09-21 22:08:29 +0200

    chore: change cgan

--- models/cgan.py
+++ models/cgan.py
@@ -70,9 +70,7 @@ class CGAN(pl.LightningModule):
             normalize=True,
         )
 
-        self.logger.experiment.add_image(
-            "Generated_Images", grid, self.current_epoch
-        )
+        self.logger.experiment.add_image("Generated_Images", grid, self.current_epoch)
 
         if self.hparams["d_type"] == "vagan":
             pred_fake = self.discriminator(generated_fences, for_G=True)
@@ -140,10 +138,7 @@ class CGAN(pl.LightningModule):
         self.manual_backward(loss_D)
         optimizer_D.step()
 
-        if not self.calculate_scores_during_training:
-            return
-
-    def on_train_epoch_end(self):
+    def validation_step(self, batch, batch_idx):
         # if (
         #     not self.current_epoch % 20 == 0
         #     or self.current_epoch == 0
@@ -151,38 +146,34 @@ class CGAN(pl.LightningModule):
         # ):
         #     return
 
-        val_dataloader = self.trainer.datamodule.val_dataloader()
-        loader_A = val_dataloader["background"]
-        loader_B = val_dataloader["fence"]
+        bg_imgs, fence_imgs = batch["background"], batch["fence"]
 
-        for batch_A, batch_B in zip(loader_A, loader_B):
+        generated_fences = self.generator(bg_imgs)
 
-            bg_imgs = batch_A.to(self.device)
-            fence_imgs = batch_B.to(self.device)
-            fake_fence_imgs = self.generator(bg_imgs)
+        grid = make_grid(
+            torch.cat((bg_imgs, generated_fences, fence_imgs), dim=0),
+            nrow=4,
+            normalize=True,
+        )
 
-            grid = make_grid(
-                torch.cat((bg_imgs, fake_fence_imgs, fence_imgs), dim=0),
-                nrow=4,
-                normalize=True,
-            )
+        self.logger.experiment.add_image("Validation_Images", grid, self.current_epoch)
 
-            self.logger.experiment.add_image(
-                "Generated_Images", grid, self.current_epoch
-            )
+        norm_fence_imgs = preprocess_for_fid(fence_imgs)
+        norm_fake_fences = preprocess_for_fid(generated_fences)
 
-            # norm_fence_imgs = preprocess_for_fid(fence_imgs)
-            # norm_fake_fences = preprocess_for_fid(fake_fence_imgs)
-            # self.fid.update(norm_fence_imgs, real=True)
-            # self.fid.update(norm_fake_fences, real=False)
-            # fid_score = self.fid.compute().item()
-            # self.log("FID", fid_score, on_epoch=True)
-            # self.fid.reset()
+        self.fid.update(norm_fence_imgs, real=True)
+        self.fid.update(norm_fake_fences, real=False)
 
-            # self.structure_loss.update_dino_struct_loss(bg_imgs, fake_fence_imgs)
-            # structure_loss = self.structure_loss.compute()
-            # self.log("DINO", structure_loss, on_epoch=True)
-            # self.structure_loss.reset()
+        self.structure_loss.update_dino_struct_loss(bg_imgs, generated_fences)
+
+    def validation_epoch_end(self, outputs):
+        fid_score = self.fid.compute().item()
+        self.log("FID", fid_score, on_epoch=True)
+        self.fid.reset()
+
+        structure_loss = self.structure_loss.compute()
+        self.log("DINO", structure_loss, on_epoch=True)
+        self.structure_loss.reset()
 
     def configure_optimizers(self):
         optimizer_G = torch.optim.AdamW(

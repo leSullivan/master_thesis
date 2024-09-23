f9ac97c953ed042734f1033ac6de47ae5682161d
John Jänckel <john.jaenckel@elunic.com>
2024-09-23 13:45:49 +0200

    434

--- main.py
+++ main.py
@@ -87,7 +87,6 @@ def main(args):
         logger=logger,
         accelerator="gpu" if torch.cuda.is_available() else "mps",
         callbacks=[checkpoint_callback],
-        strategy="ddp",
     )
 
     trainer.fit(model, data_module)

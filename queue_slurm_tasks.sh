558b3fde1515dba4a3a0c0a5200853b5fb7811ed
John Jänckel <john.jaenckel@elunic.com>
2024-09-24 22:07:20 +0200

    g

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for model_name in cgan; do
-   for g_type in unet; do 
+   for g_type in resnet-6; do 
       for d_type in basic patch pixel; do 
          sbatch --export=MODEL_NAME=$model_name,G_TYPE=$g_type,D_TYPE=$d_type slurm_template.sh
       done

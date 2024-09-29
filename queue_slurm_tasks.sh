c16f202bc863ec1419b03d0e60fddf858b3b9f82
John Jänckel <john.jaenckel@elunic.com>
2024-09-29 15:14:32 +0200

    resnet

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,15 +1,7 @@
-# #!/bin/bash
-# for model_name in cgan; do
-#    for g_type in resnet-6 resnet-9; do 
-#       for d_type in basic patch pixel vagan; do 
-#          sbatch --export=MODEL_NAME=$model_name,G_TYPE=$g_type,D_TYPE=$d_type slurm_template.sh
-#       done
-#    done
-# done
-
+#!/bin/bash
 for model_name in cgan; do
-   for g_type in unet resnet-6; do 
-      for d_type in patch; do 
+   for g_type in resnet-6 resnet-9; do 
+      for d_type in basic patch pixel vagan; do 
          sbatch --export=MODEL_NAME=$model_name,G_TYPE=$g_type,D_TYPE=$d_type slurm_template.sh
       done
    done

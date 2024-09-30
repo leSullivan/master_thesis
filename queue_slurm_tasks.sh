84477394d174e0971c489efa4db958f10dab85b4
John Jänckel <john.jaenckel@elunic.com>
2024-09-30 18:19:10 +0200

    rwerw

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-for model_name in cgan; do
-   for d_type in vagan; do 
-      for g_type in resnet-6 resnet-9 unet_256; do 
-         for lambda_cycle in 0.1 1 10 20; do 
-            sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type, slurm_template.sh
-         done
+for model_name in cyclegan; do
+   for g_type in resnet-6 resnet-9 unet_256; do 
+      for lambda_cycle in 0.1 1 10 20; do 
+         sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=vagan, slurm_template.sh
       done
    done
 done

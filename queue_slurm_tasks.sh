5ccf8b8cebeffbf2dc6a0fa3c6618cf21919a9ef
John Jänckel <john.jaenckel@elunic.com>
2024-10-02 17:37:00 +0200

    percept

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,8 +1,13 @@
-!/bin/bash
-for model_name in cyclegan; do
-   for g_type in resnet-6 resnet-9; do 
-      for lambda_cycle in 0.1 1 10 20; do 
-         sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=vagan, slurm_template.sh
+#!/bin/bash
+model_name="cgan"
+lambda_cycle="0"
+
+for d_type in patch vagan; do
+   for g_type in resnet-6 unet_256; do 
+      for lambda_perceptual in 1 5 10; do 
+         for lambda_gan in 0.5 1 2; do 
+            sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan slurm_template.sh
+         done 
       done
    done
 done

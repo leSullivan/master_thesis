3222817f530233ac29cd2b9ddfd7765da33ba37a
John Jänckel <john.jaenckel@elunic.com>
2024-10-02 17:53:23 +0200

    fponrf

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-model_name="turbo_cyclegan"
+model_name="cyclegan"
 lambda_cycle="0"
-g_type="resnet-6"
-d_type="patch"
+# g_type="resnet-6"
+# d_type="patch"
 lambda_perceptual="5"
 lambda_gan="0.5"
+lambda_cycle="10"
 
 
-
-# for d_type in patch vagan; do
-#    for g_type in resnet-6 unet_256; do 
-#       for lambda_perceptual in 1 5 10; do 
-#          for lambda_gan in 0.5 1 2; do 
-sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan slurm_template.sh
-#          done 
-#       done
-#    done
-# done
+for g_type in resnet-6 unet_256; do 
+   for d_type in patch vagan; do
+         sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan slurm_template.sh
+   done
+done

abea7e4be33393b2c37a120ff55e629eec7e3db9
John Jänckel <john.jaenckel@elunic.com>
2024-10-19 14:27:13 +0200

    stanstanstan

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-model_name="cyclegan"
+model_name="cgan"
 lambda_cycle="0"
 # g_type="resnet-6"
 # d_type="patch"
@@ -8,7 +8,7 @@ lambda_gan="0.5"
 lambda_cycle="10"
 
 
-for g_type in resnet-6 unet_256; do 
+for g_type in stan_unet; do 
    for d_type in patch vagan; do
          sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan slurm_template.sh
    done

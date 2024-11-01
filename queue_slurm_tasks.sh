0ba7afa8e97468a81134da6aced18bac0b5a9628
John Jänckel <john.jaenckel@elunic.com>
2024-11-01 11:24:43 +0100

    queueueu

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -10,18 +10,18 @@ crop=True
 
 
 
-for model_name in cgan; do 
-  for g_type in unet_128 unet_256 resnet-6 resnet-skip-con; do
-    for d_type in patch vagan; do
-      for ngf in 64 128; do
-        if [ $model_name == "turbo_cyclegan" ]; then
-          sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan,NGF=$ngf,CROP=$crop slurm_multigpu_template.sh
-        else
-          sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan,NGF=$ngf,CROP=$crop slurm_template.sh
-        fi
-      done
-    done
-  done
-done
+# for model_name in cgan; do 
+#   for g_type in unet_128 unet_256 resnet-6 resnet-skip-con; do
+#     for d_type in patch vagan; do
+#       for ngf in 64 128; do
+#         if [ $model_name == "turbo_cyclegan" ]; then
+#           sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan,NGF=$ngf,CROP=$crop slurm_multigpu_template.sh
+#         else
+#           sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan,NGF=$ngf,CROP=$crop slurm_template.sh
+#         fi
+#       done
+#     done
+#   done
+# done
 
 sbatch --export=MODEL_NAME=$model_name,LAMBDA_CYCLE=$lambda_cycle,G_TYPE=$g_type,D_TYPE=$d_type,LAMBDA_PERCEPTUAL=$lambda_perceptual,LAMBDA_GAN=$lambda_gan,NGF=$ngf,CROP=$crop slurm_multigpu_template.sh

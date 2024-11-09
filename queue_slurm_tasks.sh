4969879e91922995560c4c165c08e17bb5ccfdf3
John Jänckel <john.jaenckel@elunic.com>
2024-11-09 12:23:19 +0100

    ddd

--- queue_slurm_tasks.sh
+++ queue_slurm_tasks.sh
@@ -6,12 +6,16 @@ lambda_perceptual="5"
 lambda_gan="0.5"
 lambda_cycle="10"
 ngf=64
-crop=True
+crop=False
 
+# next experiments
 
+# crop false
+# cycle gan with best results in cropped and not cropped
+# get turbo turbo cycle gan working
 
 for model_name in cgan; do 
-  for g_type in unet_128 unet_256 resnet-6 resnet-skip-con; do
+  for g_type in resnet-6 resnet-skip-con; do
     for d_type in patch vagan; do
       for ngf in 64 128; do
         for lambda_perceptual in 1 5 10; do

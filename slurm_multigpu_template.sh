f7c2f02680e7146f8213163041d81cb40ede1389
John Jänckel <john.jaenckel@elunic.com>
2024-11-28 00:08:13 +0100

    plssss work

--- slurm_multigpu_template.sh
+++ slurm_multigpu_template.sh
@@ -11,10 +11,31 @@
 #SBATCH --time=48:00:00
 
 # Load necessary modules
-export NCCL_DEBUG=INFO
-
-source python_env/bin/activate
 module load NCCL
 
+# Set up NCCL environment
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=0
+export NCCL_NET_GDR_DISABLE=0
+export NCCL_IB_CUDA_SUPPORT=1
+
+# Set up distributed training environment variables
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export MASTER_ADDR=$(hostname)
+export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+
+# Activate virtual environment
+source python_env/bin/activate
+
 # Run your training script
-python main.py --model_name=$MODEL_NAME --g_type=$G_TYPE --d_type=$D_TYPE --lambda_cycle=$LAMBDA_CYCLE --lambda_perceptual=$LAMBDA_PERCEPTUAL --lambda_gan=$LAMBDA_GAN --ngf=$NGF --crop=$CROP
+srun python main.py \
+    --model_name=$MODEL_NAME \
+    --g_type=$G_TYPE \
+    --d_type=$D_TYPE \
+    --lambda_cycle=$LAMBDA_CYCLE \
+    --lambda_perceptual=$LAMBDA_PERCEPTUAL \
+    --lambda_gan=$LAMBDA_GAN \
+    --ngf=$NGF \
+    --crop=$CROP
